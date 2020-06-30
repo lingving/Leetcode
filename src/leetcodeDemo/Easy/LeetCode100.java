2b569b36305aa914e646a160d903c728f4d24ff1
lingving <18672542238@163.com>
2020-06-30 19:37:49 +0800

    leetcode 100

--- src/leetcodeDemo/Easy/LeetCode100.java
+++ src/leetcodeDemo/Easy/LeetCode100.java
@@ -7,4 +7,16 @@ package leetcodeDemo.Easy;
  * @create: 2020-06-30 19:24
  **/
 public class LeetCode100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null) return false;
+        if(q == null) return false;
+        if(p.val == q.val){
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }else{
+            return false;
+        }
+
+    }
 }
--- src/leetcodeDemo/Easy/TreeNode.java
+++ src/leetcodeDemo/Easy/TreeNode.java
@@ -7,4 +7,8 @@ package leetcodeDemo.Easy;
  * @create: 2020-06-30 19:23
  **/
 public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
 }
