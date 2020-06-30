52e08e665fd14a00729982d82261d01c1247f5e5
lingving <18672542238@163.com>
2020-06-30 19:35:28 +0800

    leetcode 100

---
+++ src/leetcodeDemo/Easy/LeetCode100.java
@@ -0,0 +1,10 @@
+package leetcodeDemo.Easy;
+
+/**
+ * @program: Leetcode
+ * @description: Given two binary trees, write a function to check if they are the same or not.
+ * @author: lingving
+ * @create: 2020-06-30 19:24
+ **/
+public class LeetCode100 {
+}
---
+++ src/leetcodeDemo/Easy/TreeNode.java
@@ -0,0 +1,10 @@
+package leetcodeDemo.Easy;
+
+/**
+ * @program: Leetcode
+ * @description:
+ * @author: lingving
+ * @create: 2020-06-30 19:23
+ **/
+public class TreeNode {
+}
