3b1a057e97ae1cbb7b2302b997e0e8c400137fa6
lingving <1934257349@qq.com>
2020-08-15 22:50:33 +0800

    add Jz06

---
+++ src/JianZhiOffer/Jz06.java
@@ -0,0 +1,20 @@
+package JianZhiOffer;
+
+
+/**
+ * @program: Leetcode
+ * @description: 从尾到头打印链表
+ * @author: lingving
+ * @create: 2020-08-15 22:39
+ **/
+public class Jz06 {
+    public int[] reversePrint(ListNode head) {
+        ListNode cur = head ;
+        int count = 0;
+        while (cur != null){
+            cur = cur.next;
+            count
+        }
+
+    }
+}
