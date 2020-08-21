876f286332d01c5972cfe3e6a981976f5699c861
lingving <1934257349@qq.com>
2020-08-21 23:35:41 +0800

    add Jz 24,32

---
+++ src/JianZhiOffer/Jz24.java
@@ -0,0 +1,10 @@
+package JianZhiOffer;
+
+/**
+ * @program: Leetcode
+ * @description: 反转链表
+ * @author: lingving
+ * @create: 2020-08-21 23:02
+ **/
+public class Jz24 {
+}
---
+++ src/JianZhiOffer/Jz32.java
@@ -0,0 +1,10 @@
+package JianZhiOffer;
+
+/**
+ * @program: Leetcode
+ * @description: 从上到下打印二叉树 II
+ * @author: lingving
+ * @create: 2020-08-21 23:08
+ **/
+public class Jz32 {
+}
