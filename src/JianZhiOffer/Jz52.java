e43b9ccae17bb5008c1cb6d7d38c8124e729a08e
lingving <1934257349@qq.com>
2020-08-26 23:27:44 +0800

    add jz 52,53

---
+++ src/JianZhiOffer/Jz52.java
@@ -0,0 +1,10 @@
+package JianZhiOffer;
+
+/**
+ * @program: Leetcode
+ * @description: 两个链表的第一个公共节点
+ * @author: lingving
+ * @create: 2020-08-26 23:03
+ **/
+public class Jz52 {
+}
---
+++ src/JianZhiOffer/Jz53.java
@@ -0,0 +1,10 @@
+package JianZhiOffer;
+
+/**
+ * @program: Leetcode
+ * @description: 在排序数组中查找数字 I
+ * @author: lingving
+ * @create: 2020-08-26 23:07
+ **/
+public class Jz53 {
+}
