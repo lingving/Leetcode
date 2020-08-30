e211d79f7d24575901c02e78dcada77e697a3b78
lingving <1934257349@qq.com>
2020-08-30 22:24:40 +0800

    add leetcode 40

---
+++ src/leetcodeDemo/LeetCode39.java
@@ -0,0 +1,10 @@
+package leetcodeDemo;
+
+/**
+ * @program: Leetcode
+ * @description: 组合总和
+ * @author: lingving
+ * @create: 2020-08-30 08:20
+ **/
+public class LeetCode39 {
+}
---
+++ src/leetcodeDemo/LeetCode40.java
@@ -0,0 +1,10 @@
+package leetcodeDemo;
+
+/**
+ * @program: Leetcode
+ * @description: 组合总和 II
+ * @author: lingving
+ * @create: 2020-08-30 09:51
+ **/
+public class LeetCode40 {
+}
